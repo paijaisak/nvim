11385949c1c9410918625a3e6eefeaedc5f3c752
Paijá <paijaisak@outlook.com>
2025-03-13 17:22:45 +0100

    remove nvim background to match terminal

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,17 +16,25 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
+
+{
+},
     -- import your plugins
-    { import = "plugins" },
+ --   { import = "plugins" },
   },
+
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+
